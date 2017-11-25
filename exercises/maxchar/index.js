8fcdd8a3e1178201b3873127e532ac1eadf4e61e
VladFiliucov <filiucov@mail.ru>
2017-11-25 13:01:50 +0000

    Solve palindrome and maxchar.

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,27 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const arr = [...str]
+  const obj = arr.reduce((acc, el) => {
+    const element = acc[el] ? (acc[el] + 1) : 1
+
+    return Object.assign(acc, {[el]: element})
+  } , {})
+
+  let max = 0;
+  let winner = '';
+
+  for (let prop in obj) {
+    if (obj[prop] > max) {
+      max = obj[prop]
+      winner = prop
+    }
+  }
+
+  return winner;
+}
+
+console.log(maxChar("abcccccccd"))
 
 module.exports = maxChar;
--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,13 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  if(n >= 0) {
+    return parseInt(n.toString().split('').reverse().join(''));
+  } else if (n < 0) {
+    const arr = "-" + n.toString().split('').reverse().filter(x => x != '-').join('');
+    return parseInt(arr);
+  }
+}
 
 module.exports = reverseInt;
